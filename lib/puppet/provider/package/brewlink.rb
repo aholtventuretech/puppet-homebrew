979668136228b70cd48aa4d1e13c91104db33ffc
Alan Holt <aholt@venturetech.net>
2017-02-24 16:08:01 -0600

    added brew link package provider

---
+++ lib/puppet/provider/package/brewlink.rb
@@ -0,0 +1,38 @@
+Puppet::Type.type(:package).provide(:brewlink,
+                                    :parent => :a_brewcommon,
+                                    :source => :a_brewcommon) do
+  desc 'Brew Link management using HomeBrew on OS X'
+
+  has_feature :install_options
+
+  def install
+    name = @resource[:name].downcase
+
+    Puppet.debug "Linking #{name}"
+    output = execute([command(:brew), :link, '-f', '--overwrite', name, *install_options])
+
+    if output =~ /Error: No such keg/
+      raise Puppet::Error, "Could not find keg #{name}"
+    end
+  end
+
+  def uninstall
+    name = @resource[:name].downcase
+
+    Puppet.debug "Unlinking #{name}"
+    execute([command(:brew), :unlink, name])
+  end
+
+  def update
+    uninstall
+    install
+  end
+
+  def query
+    nil
+  end
+
+  def self.instances
+    nil
+  end
+end
