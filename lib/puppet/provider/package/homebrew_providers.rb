a6eaf05ae2bd4d0634e76618bae5c38d752eb26f
Alan Holt <aholt@venturetech.net>
2017-02-28 09:29:11 -0600

    updated to explicitly load providers in order

---
+++ lib/puppet/provider/package/homebrew_providers.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# noinspection RubyResolve
+class HomebrewProviders
+  def load_providers
+    current_dir = File.expand_path(File.dirname(__FILE__))
+
+    require "#{current_dir}/../../../../includes/providers/a_brewcommon"
+    require "#{current_dir}/../../../../includes/providers/brew"
+    require "#{current_dir}/../../../../includes/providers/brewcask"
+    require "#{current_dir}/../../../../includes/providers/brewlink"
+    require "#{current_dir}/../../../../includes/providers/homebrew"
+    require "#{current_dir}/../../../../includes/providers/tap"
+  end
+end
+
+HomebrewProviders.new.load_providers
